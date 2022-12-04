2a892f5f46561f8348aef1570e2aafe14264cfbe
Coen Zeng <c36zeng@uwaterloo.ca>
2022-12-04 02:22:08 -0500

    update computer2

--- Player/computer2.cc
+++ Player/computer2.cc
@@ -3,4 +3,41 @@
 Computer2::Computer2(bool isWhite): isWhite{isWhite} {};
 Computer2::~Computer2(){};
 bool Computer2::makeMove(Board * board)
-{return true;};
+{
+    std::vector<std::tuple<int, int, int, int, bool, bool>> moves;
+
+    if (isWhite){
+        moves = board->getAllLegalWhiteMoves();
+    } else {
+        moves = board->getAllLegalBlackMoves();
+    }
+
+    int startRow;
+    int startCol;
+    int endRow;
+    int endCol;
+
+    //search for capture move
+    for (size_t index = 0; index < moves.size(); index++){
+        if (std::get<4>(moves[index])){ //the 4th index is a boolean for isCapture
+            startRow = std::get<0>(moves[index]);
+            startCol = std::get<1>(moves[index]);
+            endRow = std::get<2>(moves[index]);
+            endCol = std::get<3>(moves[index]);
+            board->makeMove(startRow, startCol, endRow, endCol);
+            return true;
+        };
+    }
+    
+    //if there are no capture moves, pick a random legal move 
+
+    int randomIndex = rand() % moves.size(); //generate a random number between [0, moves.size() - 1]
+    
+    startRow = std::get<0>(moves[randomIndex]);
+    startCol = std::get<1>(moves[randomIndex]);
+    endRow = std::get<2>(moves[randomIndex]);
+    endCol = std::get<3>(moves[randomIndex]);
+
+    board->makeMove(startRow, startCol, endRow, endCol);
+    return true;
+};
